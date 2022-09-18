b7297784043816d0c9d84e5d1393c1ef8ada6db8
lmesh91 <92399923+lmesh91@users.noreply.github.com>
2022-09-18 18:56:50 -0400

    Shared memory, general optimizations

--- collatz.cu
+++ collatz.cu
@@ -3,48 +3,123 @@
 #include <algorithm>
 #include <stdint.h>
 #include <math.h>
+#include <chrono>
+#include <thread>
 __global__
-void collatzGPURepeatedSteps(int repeatDepth, long long sieveSize, long long *c, long long *s, long long *t, long long *cycleLeads, long long *cycleLeadBounds, long long *cycleLeadDelays, long long SHARED_MEM_LIMIT, long long minN, long long maxN) {
+void collatzGPURepeatedSteps(int repeatDepth, int sharedMemLevel, long long sieveSize, long long *c, long long *s, char *t, long long *cycleLeads, long long *cycleLeadBounds, char *cycleLeadDelays, long long minN, long long maxN) {
     long long d_repeated = 0;
-    long long cycleLeadMax = cycleLeadBounds[1];
-    long long cycleLeadSize = cycleLeadBounds[2]-1;
-    extern __shared__ long long sharedCycLeads[];
-    for (int i = threadIdx.x; i < SHARED_MEM_LIMIT; i += blockDim.x) {
-        sharedCycLeads[i] = cycleLeads[i];
-    }
-    long long sharedCycLeadMax = sharedCycLeads[SHARED_MEM_LIMIT-1];
-    __syncthreads();
+    //__shared__ long long sharedCycLeads[];
     //long long cycleLeadMin = cycleLeadBounds[0];
-    for (long long i = minN+threadIdx.x+blockIdx.x*blockDim.x; i <= maxN; i+=blockDim.x*gridDim.x) {
-        long long x = i;
-        while (x != 1) {
-            if (x <= cycleLeadMax) { //add x > cycleLeadBounds[0] when doing mesh-collatz
-                if (SHARED_MEM_LIMIT - 1 == cycleLeadSize) {
+    __shared__ long long pows3[40];
+    if (threadIdx.x == 0) {
+        pows3[0] = 1;
+        for (int i = 1; i < 40; i++) {
+            pows3[i] = pows3[i-1] * 3;
+        }
+    }
+    long long cycleLeadMax = cycleLeadBounds[1];
+    int cycleLeadSize = cycleLeadBounds[2]-1;
+    if (sharedMemLevel == 2) { //Shared Cycle Leads *and* Sieve
+        const int sharedSize = 512;
+        __shared__ long long sharedCycleLeads[sharedSize];
+        __shared__ char sharedCycleLeadDelays[sharedSize];
+        for (int i = threadIdx.x; i < cycleLeadSize; i += blockDim.x) {
+            sharedCycleLeads[i] = cycleLeads[i];
+            sharedCycleLeadDelays[i] = cycleLeadDelays[i];
+        }
+        const int sharedSieveSize = 2048;
+        __shared__ long long sharedS[sharedSieveSize];
+        __shared__ char sharedT[sharedSieveSize];
+        for (int i = threadIdx.x; i < sieveSize; i++) {
+            sharedS[i] = s[i];
+            sharedT[i] = t[i];
+        }
+        __syncthreads();
+        for (long long i = minN+threadIdx.x+blockIdx.x*blockDim.x; i <= maxN; i+=blockDim.x*gridDim.x) {
+            long long x = i;
+            while (x != 1) {
+                if (x <= cycleLeadMax) { //add x > cycleLeadBounds[0] when doing mesh-collatz
                     int low = 0;
-                    int high = SHARED_MEM_LIMIT - 1;  
+                    int high = cycleLeadSize;  
                     while (low != high) {
-                        float partPercent = 0.5;
-                        int mid = partPercent*(low)+(1-partPercent)*(high-1);
-                        if (x == sharedCycLeads[mid]) {
-                            d_repeated += cycleLeadDelays[mid];
-                            goto endOfLoop;
-                        } else if (x > sharedCycLeads[mid]) {
+                        int mid = (low+high)/2;
+                        if (x == sharedCycleLeads[mid]) {
+                            d_repeated += sharedCycleLeadDelays[mid];
+                            goto endOfLoop2;
+                        } else if (x > sharedCycleLeads[mid]) {
                             low = mid + 1;
                         } else {
                             high = mid;
                         }
                     }
+                }
+                d_repeated+=repeatDepth+t[x%sieveSize];
+                //todo: account for 64-bit overflow, e.g. n = 8528817511 goes over 64-bits, but is lucky enough to not do that in one go
+                if (x >= sieveSize || x < 0) {
+                    /*if ((INT64_MAX - s[x%sieveSize])/powf(3,t[x%sieveSize]) < x/sieveSize) {
+                        printf("Warning: Overflow\nn = %llu, x = %llu, calculating %llu*%llu+%llu\n", i, x, (long long)powf(3,t[x%sieveSize]), x/sieveSize, s[x%sieveSize]);
+                    }*/
+                    x = pows3[sharedT[x%sieveSize]]*(x/sieveSize)+sharedS[x%sieveSize];
                 } else {
+                    x = sharedS[x];
+                }
+            }
+            endOfLoop2:;
+        }
+    }
+    if (sharedMemLevel == 1) { //Shared Cycle Leads, Unified Sieve
+        const int sharedSize = 2048;
+        __shared__ long long sharedCycleLeads[sharedSize];
+        __shared__ char sharedCycleLeadDelays[sharedSize];
+        for (int i = threadIdx.x; i < cycleLeadSize; i += blockDim.x) {
+            sharedCycleLeads[i] = cycleLeads[i];   
+            sharedCycleLeadDelays[i] = cycleLeadDelays[i];
+        }
+        __syncthreads();
+        for (long long i = minN+threadIdx.x+blockIdx.x*blockDim.x; i <= maxN; i+=blockDim.x*gridDim.x) {
+            long long x = i;
+            while (x != 1) {
+                if (x <= cycleLeadMax) { //add x > cycleLeadBounds[0] when doing mesh-collatz
                     int low = 0;
-                    int high = cycleLeadSize;
-                    printf("%i < %i, %llu\n", low, high, sharedCycLeadMax);  
-                        goto endOfLoop;
+                    int high = cycleLeadSize;  
                     while (low != high) {
-                        float partPercent = 0.5;
-                        int mid = partPercent*(low)+(1-partPercent)*(high-1);
+                        int mid = (low+high)/2;
+                        if (x == sharedCycleLeads[mid]) {
+                            d_repeated += sharedCycleLeadDelays[mid];
+                            goto endOfLoop1;
+                        } else if (x > sharedCycleLeads[mid]) {
+                            low = mid + 1;
+                        } else {
+                            high = mid;
+                        }
+                    }
+                }
+                d_repeated+=repeatDepth+t[x%sieveSize];
+                //todo: account for 64-bit overflow, e.g. n = 8528817511 goes over 64-bits, but is lucky enough to not do that in one go
+                if (x >= sieveSize || x < 0) {
+                    /*if ((INT64_MAX - s[x%sieveSize])/powf(3,t[x%sieveSize]) < x/sieveSize) {
+                        printf("Warning: Overflow\nn = %llu, x = %llu, calculating %llu*%llu+%llu\n", i, x, (long long)powf(3,t[x%sieveSize]), x/sieveSize, s[x%sieveSize]);
+                    }*/
+                    x = pows3[t[x%sieveSize]]*(x/sieveSize)+s[x%sieveSize];
+                } else {
+                    x = s[x];
+                }
+            }
+            endOfLoop1:;
+        }
+    }
+    if (sharedMemLevel == 0) { //Unified Cycle Leads and Sieve
+        for (long long i = minN+threadIdx.x+blockIdx.x*blockDim.x; i <= maxN; i+=blockDim.x*gridDim.x) {
+            long long x = i;
+            while (x != 1) {
+                if (x <= cycleLeadMax) { //add x > cycleLeadBounds[0] when doing mesh-collatz
+                    int low = 0;
+                    int high = cycleLeadSize;  
+                    while (low != high) {
+                        int mid = (low+high)/2;
                         if (x == cycleLeads[mid]) {
                             d_repeated += cycleLeadDelays[mid];
-                            goto endOfLoop;
+                            goto endOfLoop0;
                         } else if (x > cycleLeads[mid]) {
                             low = mid + 1;
                         } else {
@@ -52,29 +127,25 @@ void collatzGPURepeatedSteps(int repeatDepth, long long sieveSize, long long *c,
                         }
                     }
                 }
-            }
-            d_repeated+=repeatDepth+t[x%sieveSize];
-            //todo: account for 64-bit overflow, e.g. n = 8528817511 goes over 64-bits, but is lucky enough to not do that in one go
-            if (x >= sieveSize || x < 0) {
-                /*if ((INT64_MAX - s[x%sieveSize])/powf(3,t[x%sieveSize]) < x/sieveSize) {
-                    printf("Warning: Overflow\nn = %llu, x = %llu, calculating %llu*%llu+%llu\n", i, x, (long long)powf(3,t[x%sieveSize]), x/sieveSize, s[x%sieveSize]);
-                }*/
-                long long pow3 = 1;
-                for (int i = 0; i < t[x%sieveSize]; i++) {
-                    pow3 *= 3;
+                d_repeated+=repeatDepth+t[x%sieveSize];
+                //todo: account for 64-bit overflow, e.g. n = 8528817511 goes over 64-bits, but is lucky enough to not do that in one go
+                if (x >= sieveSize || x < 0) {
+                    /*if ((INT64_MAX - s[x%sieveSize])/powf(3,t[x%sieveSize]) < x/sieveSize) {
+                        printf("Warning: Overflow\nn = %llu, x = %llu, calculating %llu*%llu+%llu\n", i, x, (long long)powf(3,t[x%sieveSize]), x/sieveSize, s[x%sieveSize]);
+                    }*/
+                    x = pows3[t[x%sieveSize]]*(x/sieveSize)+s[x%sieveSize];
+                } else {
+                    x = s[x];
                 }
-                x = pow3*(x/sieveSize)+s[x%sieveSize];
-            } else {
-                x = s[x];
             }
+            endOfLoop0:;
         }
-        endOfLoop:;
     }
     c[threadIdx.x+blockIdx.x*blockDim.x]=d_repeated;
 }
 
 float collatzRepeatedSteps(int repeatDepth, long long N, int blocks, int threads) {
-    std::cout << "Testing up to n = " << std::to_string(N) << std::endl;
+    //std::cout << "Testing up to n = " << std::to_string(N) << std::endl;
     long long *c;
     cudaMallocManaged(&c, sizeof(long long)*blocks*threads);
     cudaEvent_t start, stop;
@@ -84,7 +155,7 @@ float collatzRepeatedSteps(int repeatDepth, long long N, int blocks, int threads
     //Find the numbers that go to 1 within repeatDepth steps.
     long long *cycleLeads;
     cudaMallocManaged(&cycleLeads, sizeof(long long)*100000000);
-    long long *cycleLeadDelays;
+    char *cycleLeadDelays;
     cycleLeads[0] = 1;
     long long cycleLeadNext = 1;
     long long cycleLeadNow = 1;
@@ -93,7 +164,7 @@ float collatzRepeatedSteps(int repeatDepth, long long N, int blocks, int threads
     cudaMallocManaged(&cycleLeadBounds, sizeof(long long)*3);
     cycleLeadBounds[0] = 1;
     cycleLeadBounds[1] = 1;
-    printf("Generating cycle leads\n");
+    //printf("Generating cycle leads\n");
     for (int iters = 0; iters < repeatDepth; iters++) {
         for (int i = cycleLeadPrev; i < cycleLeadNow; i++) {
             cycleLeads[cycleLeadNext] = cycleLeads[i] << 1; cycleLeadNext++;
@@ -119,7 +190,7 @@ float collatzRepeatedSteps(int repeatDepth, long long N, int blocks, int threads
     cycleLeadBounds[2] = cycleLeadNow;
 
     std::sort(cycleLeads, cycleLeads + cycleLeadNow);
-    cudaMallocManaged(&cycleLeadDelays, sizeof(long long)*100000000);
+    cudaMallocManaged(&cycleLeadDelays, sizeof(char)*100000000);
     cycleLeadDelays[0] = 1;
     for (int i = 0; i < cycleLeadNow; i++) {
         long long x = cycleLeads[i];
@@ -135,18 +206,14 @@ float collatzRepeatedSteps(int repeatDepth, long long N, int blocks, int threads
         }
     }
 
-    long long origMemLimit = 1024;
-    if (origMemLimit > cycleLeadNow) {
-        origMemLimit = cycleLeadNow;
-    }
-    const long long SHARED_MEM_LIMIT = origMemLimit;
 
-    printf("Generating Sieve\n");
+    //printf("Generating Sieve\n");
     //Now we generate the sieve.
     //S saves the trajectory, T saves the number of 3x+1 steps. We can show every number is equal to 3^T*(x/2^r)+S
-    long long *s, *t;
+    long long *s;
+    char *t;
     cudaMallocManaged(&s, sizeof(long long)<<repeatDepth);
-    cudaMallocManaged(&t, sizeof(long long)<<repeatDepth);
+    cudaMallocManaged(&t, sizeof(char)<<repeatDepth);
     //Todo: Do this on GPU
     for (long long i = 0; i < 1<<repeatDepth; i++) {
         long long x = i;
@@ -164,9 +231,18 @@ float collatzRepeatedSteps(int repeatDepth, long long N, int blocks, int threads
     for (int i = 0; i < blocks*threads; i++) {
         c[i] = 0;
     }
-    printf("Starting\n");
+    //printf("Starting\n");
+
+    int sharedMemLevel = 0;
+    if (2048 <= cycleLeadNow) {
+        sharedMemLevel = 1;
+    }
+    if (512 <= cycleLeadNow && repeatDepth <= 11) {
+        sharedMemLevel = 2;
+    }
+
     cudaEventRecord(start);
-    collatzGPURepeatedSteps<<<blocks, threads, SHARED_MEM_LIMIT * sizeof(long long)>>>(repeatDepth, 1<<repeatDepth, c, s, t, cycleLeads, cycleLeadBounds, cycleLeadDelays, SHARED_MEM_LIMIT, 1, N);
+    collatzGPURepeatedSteps<<<blocks, threads>>>(repeatDepth, sharedMemLevel, 1<<repeatDepth, c, s, t, cycleLeads, cycleLeadBounds, cycleLeadDelays, 1, N);
     cudaEventRecord(stop);
 
 
@@ -179,6 +255,11 @@ float collatzRepeatedSteps(int repeatDepth, long long N, int blocks, int threads
         delayTot += c[i];
     }
     cudaFree(c);
+    cudaFree(s);
+    cudaFree(t);
+    cudaFree(cycleLeads);
+    cudaFree(cycleLeadBounds);
+    cudaFree(cycleLeadDelays);
     float milliseconds = 0;
     cudaEventElapsedTime(&milliseconds, start, stop);
     std::cout << "Elapsed time: " << std::to_string(milliseconds) << "ms" << std::endl;
@@ -249,8 +330,12 @@ void bench(long long N, int blocks, int threads, int reps) {
 
 
 int main() {
-    std::cout << "Mesh-Collatz Searcher v0.0.3" << std::endl;
+    std::cout << "Mesh-Collatz Searcher v0.1.0\nRunning Benchmark..." << std::endl;
     long long N = 10000000000LL;
-    collatzShortcut(N, 256, 512);
-    collatzRepeatedSteps(20, N, 256, 512); //switch to 256x512 when stable
+    printf("Warmup\n");
+    collatzRepeatedSteps(1, N, 512, 512);
+    printf("No Sieve\n");
+    collatzShortcut(N, 512, 512);
+    printf("2^18 Sieve:\n");
+    collatzRepeatedSteps(18, N, 512, 512);
 }
